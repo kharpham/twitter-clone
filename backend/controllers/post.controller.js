cc56949fa15e21aca99c9e9bbc65693014ac45c3
Khapham <khar22102003@gmail.com>
2024-10-22 11:18:03 +0700

    Populate data when fetching all posts

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -118,7 +118,16 @@ export const likeUnlikePost = async (req, res) => {
 
 export const getAllPosts = async (req, res) => {
     try {
-        const posts = await Post.find().sort({createdAt: -1});
+        const posts = await Post.find().sort({createdAt: -1}).populate({
+            path: "user",
+            select: "fullname", 
+        }).populate({
+            path: "comments",
+            populate: {
+                path: "user",
+                select: "fullname",
+            }
+        });
         if (posts.length === 0) return res.status(200).json([]);
         return res.status(200).json(posts);
     } catch (error) {
