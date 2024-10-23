d4aa3054a986aead7d74d0224806f18bff6a4ba1
Khapham <khar22102003@gmail.com>
2024-10-23 20:59:26 +0700

    Add common components and post-related components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from "./components/common/SideBar";
+import RightPanel from "./components/common/RightPanel";
 
 function App() {
   return (
     <>
       <div className="flex max-w-6xl mx-auto">
+        {/* Not wrapped with routes => common component */}
+        <Sidebar/> 
         <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/login" element={<LoginPage/>}/>
           <Route path="/signup" element={<SignUpPage/>}/>
         </Routes>
+        <RightPanel/>
       </div>
     </>
   )
