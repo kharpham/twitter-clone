db10903f38e73e827a62c34ecaf6696f396fee0d
Khapham <khar22102003@gmail.com>
2024-11-13 16:37:20 +0700

    Add Suggested users Panel

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
-
+import { useQuery } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 const RightPanel = () => {
-	const isLoading = false;
-
+	const {data: suggestedUser, isLoading} = useQuery({
+		queryKey: ['suggestedUser'],
+		queryFn: async () => {
+			try {
+				const res = await fetch("/api/users/suggested");
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error || "Something went wrong");
+				return data;
+			} catch (error) {
+				console.log(error);
+				toast.error(error.message);
+			}
+		}
+	});
+	if (suggestedUser?.length === 0) return <div className="md:w-64 w-0"></div>;
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
 			<div className='bg-[#16181C] p-4 rounded-md sticky top-2'>
@@ -20,7 +33,7 @@ const RightPanel = () => {
 						</>
 					)}
 					{!isLoading &&
-						USERS_FOR_RIGHT_PANEL?.map((user) => (
+						suggestedUser?.map((user) => (
 							<Link
 								to={`/profile/${user.username}`}
 								className='flex items-center justify-between gap-4'
@@ -34,7 +47,7 @@ const RightPanel = () => {
 									</div>
 									<div className='flex flex-col'>
 										<span className='font-semibold tracking-tight truncate w-28'>
-											{user.fullName}
+											{user.fullname}
 										</span>
 										<span className='text-sm text-slate-500'>@{user.username}</span>
 									</div>
