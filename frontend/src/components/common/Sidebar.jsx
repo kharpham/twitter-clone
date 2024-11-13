f613994b093d1b6068a831ccdc738801875ea80d
Khapham <khar22102003@gmail.com>
2024-11-13 11:12:29 +0700

    Add signup/login/logout fetching in frontend

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -5,8 +5,30 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const Sidebar = () => {
+
+	const {mutate, isPending, isError, error} = useMutation({
+		mutationFn: async() => {
+			try {
+				const res = await fetch("/api/auth/logout", {
+					method: "POST"
+				});
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error || "Something went wrong");
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: () => {
+			toast.success("Logout successfully");
+		},
+		onError: (error) => {
+			toast.error("Failed to logout");
+		}
+	});
 	const data = {
 		fullName: "John Doe",
 		username: "johndoe",
@@ -64,7 +86,10 @@ const Sidebar = () => {
 								<p className='text-white font-bold text-sm w-20 truncate'>{data?.fullName}</p>
 								<p className='text-slate-500 text-sm'>@{data?.username}</p>
 							</div>
-							<BiLogOut className='w-5 h-5 cursor-pointer' />
+							<BiLogOut onClick={(e) => {
+								e.preventDefault();
+								mutate();
+							}} className='w-5 h-5 cursor-pointer' />
 						</div>
 					</Link>
 				)}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast'
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
@@ -13,5 +14,6 @@ createRoot(document.getElementById('root')).render(
       <App />
       </QueryClientProvider>
     </BrowserRouter>
+    <Toaster/>
   </StrictMode>,
 )
