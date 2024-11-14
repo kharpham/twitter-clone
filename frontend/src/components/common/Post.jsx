25b1e97ea7aa814e41f4b95afc70721d2d589d45
Khapham <khar22102003@gmail.com>
2024-11-14 12:48:09 +0700

    Add time post date on posts

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import LoadingSpinner from "./LoadingSpinner";
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
@@ -116,7 +117,7 @@ const Post = ({ post }) => {
 
 	const isMyPost = post.user._id === authUser._id;
 
-	const formattedDate = "1h";
+	const formattedDate = formatPostDate(post.createdAt);
 
 	const handleDeletePost = (e) => {
 		e.preventDefault();
